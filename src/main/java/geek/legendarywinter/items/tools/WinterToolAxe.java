24128c71eccd0b374d854f9da3bab8e2f6582ae9
LegendaryGeek <timothydaley4@gmail.com>
2018-12-29 22:12:13 -0800

    tool things

--- src/main/java/geek/legendarywinter/items/tools/WinterToolAxe.java
+++ src/main/java/geek/legendarywinter/items/tools/WinterToolAxe.java
@@ -7,7 +7,6 @@ public class WinterToolAxe extends ItemAxe {
 	public WinterToolAxe() {
 		super(ToolMaterial.DIAMOND, 8.1f, 90);
 		this.setHarvestLevel("axe", 3);
-		
 		// TODO Auto-generated constructor stub
 	}
 
