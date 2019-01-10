3e671cabc8b2bcccad5ca2691d4f8b1b81c18975
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-01-11 08:18:02 +1100

    acceptable beacon payment

--- src/main/java/geek/legendarywinter/items/Polarium_Ingot.java
+++ src/main/java/geek/legendarywinter/items/Polarium_Ingot.java
@@ -1,13 +1,19 @@
 package geek.legendarywinter.items;
 
 import geek.legendarywinter.util.GeekTab;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class Polarium_Ingot extends Item {
-	
+
 	public Polarium_Ingot() {
 		super();
 		this.setCreativeTab(GeekTab.instance);
 	}
+
+	@Override
+	public boolean isBeaconPayment(final ItemStack stack) {
+		return true;
+	}
+
 }
