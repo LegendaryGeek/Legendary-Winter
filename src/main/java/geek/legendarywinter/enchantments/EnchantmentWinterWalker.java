b32f835163c5fd18beccbd64c04a642bd835950e
LegendaryGeek <timothydaley4@gmail.com>
2019-01-14 15:38:28 -0800

    changes stuff

--- src/main/java/geek/legendarywinter/enchantments/EnchantmentWinterWalker.java
+++ src/main/java/geek/legendarywinter/enchantments/EnchantmentWinterWalker.java
@@ -86,9 +86,11 @@ public class EnchantmentWinterWalker extends Enchantment {
                                 worldIn.scheduleUpdate(blockPos.toImmutable(), Blocks.MAGMA, MathHelper.getInt(living.getRNG(), 60, 120));
                             }
                         } else if (iblockstate.getBlock() == Blocks.SNOW) {
+                        	worldIn.setBlockToAir(pooledMutableBlockPos);
                             worldIn.setBlockState(pooledMutableBlockPos, Blocks.PACKED_ICE.getDefaultState());
                             worldIn.scheduleUpdate(blockPos.toImmutable(), Blocks.PACKED_ICE, MathHelper.getInt(living.getRNG(), 60, 120));
                         } else if (iblockstate.getBlock() == Blocks.STONE) {
+                        	worldIn.setBlockToAir(pooledMutableBlockPos);
                             worldIn.setBlockState(pooledMutableBlockPos, BlocksRegistry.WinterStone.getDefaultState());
                             worldIn.scheduleUpdate(blockPos.toImmutable(), BlocksRegistry.WinterStone, MathHelper.getInt(living.getRNG(), 60, 120));
                         }
--- src/main/java/geek/legendarywinter/items/tools/WinterToolPickaxe.java
+++ src/main/java/geek/legendarywinter/items/tools/WinterToolPickaxe.java
@@ -2,8 +2,13 @@ package geek.legendarywinter.items.tools;
 
 import geek.legendarywinter.LegendaryWinter;
 import geek.legendarywinter.util.GeekTab;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class WinterToolPickaxe extends ItemPickaxe {
 
@@ -13,5 +18,19 @@ public class WinterToolPickaxe extends ItemPickaxe {
 		this.setDamage(new ItemStack(this), 10);
 		this.setCreativeTab(GeekTab.instance);
 	}
+    /**
+     * Called when a Block is destroyed using this Item. Return true to trigger the "Use Item" statistic.
+     */
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving)
+    {
+        if (!worldIn.isRemote && (double)state.getBlockHardness(worldIn, pos) != 0.0D)
+        {
+        	if(state.getBlock() == Blocks.ICE) {
+        		
+        	}
+            stack.damageItem(1, entityLiving);
+        }
 
+        return true;
+    }
 }
